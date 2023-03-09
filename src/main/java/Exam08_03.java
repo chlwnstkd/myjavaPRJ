a1b0faa5c493479a42215472789ef7fb761391f3
chlwnstkd <jun1234755@naver.com>
2023-03-09 15:26:51 +0900

    commit push

---
+++ src/main/java/Exam08_03.java
@@ -0,0 +1,11 @@
+public class Exam08_03 {
+    public static void main(String[] args) {
+        boolean boo1, boo2;
+
+        boo1=true;
+        System.out.printf("%s \n", boo1);
+
+        boo2 = (10 == 20);
+        System.out.printf("%s \n", boo2);
+    }
+}
