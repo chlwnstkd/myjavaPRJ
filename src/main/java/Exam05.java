869bd45d4c18bd6c11c1e9edd9ca5eec53994fde
chlwnstkd <jun1234755@naver.com>
2023-03-09 14:05:49 +0900

    commit push

---
+++ src/main/java/Exam05.java
@@ -0,0 +1,25 @@
+public class Exam05 {
+    public static void main(String[] args) {
+
+        System.out.printf("%d\n", 123);
+        System.out.printf("%5d\n", 123);
+        System.out.printf("%05d\n", 123);
+
+        System.out.printf("%f\n", 123.45);
+        System.out.printf("%7.1f\n", 12.45);
+        System.out.printf("%7.3f\n", 123.45);
+
+        System.out.printf("%s\n", "Hi~Java");
+        System.out.printf("%10s\n", "Hi~Java");
+
+        System.out.printf("\n줄바꿈\n연습 \n");
+        System.out.printf("\t탭키\t연습 \n");
+        System.out.printf("이것을\r덮어씁니다 \n");
+        System.out.printf("글자가 \"강조\"되는 효과 \n");
+        System.out.printf("\\\\\\ 역슬래시 세개 출력 \n");
+        System.out.printf("\b뒤로\b한칸\b이동 \n");
+
+    }
+
+
+}
