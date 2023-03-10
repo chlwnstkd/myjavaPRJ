d9f20557c42b0134ea5cca98f614765fe6bad449
chlwnstkd <jun1234755@naver.com>
2023-03-10 15:11:10 +0900

    commit push

---
+++ src/main/java/baekjoon/Ex_10926.java
@@ -0,0 +1,10 @@
+package baekjoon;
+import java.util.Scanner;
+public class Ex_10926 {
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+        String str = s.nextLine();
+        System.out.println(str + "??!");
+    }
+}
