55c8b38017795b8fae7aebd8e0191405fcf9fc1e
chlwnstkd <jun1234755@naver.com>
2023-03-15 19:53:32 +0900

    commit push

---
+++ src/main/java/baekjoon_4/Ex_10818.java
@@ -0,0 +1,19 @@
+package baekjoon_4;
+
+import java.util.Arrays;
+import java.util.Scanner;
+public class Ex_10818 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int[] array = new int[n];
+
+        int i;
+
+        for (i = 0; i < n; i++) {
+            array[i] = s.nextInt();
+        }
+        System.out.println(Arrays.stream(array).min().getAsInt());
+        System.out.println(Arrays.stream(array).max().getAsInt());
+    }
+}
--- src/main/java/org/example/hello.java
+++ src/main/java/org/example/hello.java
@@ -1,3 +1,5 @@
+package org.example;
+
 public class hello {
     public static void main(String[] args) {
         System.out.println("두번째 생성한 파일");
