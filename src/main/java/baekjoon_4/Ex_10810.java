1b981b85d0537cd5ed7071e94ef9f396c6330a16
chlwnstkd <jun1234755@naver.com>
2023-03-17 18:15:57 +0900

    commit push

---
+++ src/main/java/baekjoon_4/Ex_10810.java
@@ -0,0 +1,26 @@
+package baekjoon_4;
+import java.util.Scanner;
+public class Main {
+    public static void main (String[] args) {
+        Scanner s = new Scanner(System.in);
+        int [] arr = new int [s.nextInt()];
+        int m = s.nextInt();
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = 0;
+        }
+
+        for (int j = 0; j < m; j++) {
+            int a = s.nextInt() - 1;
+            int b = s.nextInt() - 1;
+            int c = s.nextInt();
+
+            for (int k = a; k <=b; k++) {
+                arr[k] = c;
+            }
+        }
+        for (int i = 0; i < arr.length; i ++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
---
+++ src/main/java/baekjoon_4/Ex_10813.java
@@ -0,0 +1,28 @@
+package baekjoon_4;
+import java.util.Scanner;
+public class Ex_10813 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int [] arr = new int[s.nextInt()];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = i + 1;
+        }
+
+        int m = s.nextInt();
+        for (int i = 0; i < m; i++) {
+            int a = s.nextInt() - 1;
+            int b = s.nextInt() - 1;
+
+
+                int temp = arr[a];
+                arr[a] = arr[b];
+                arr[b] = temp;
+
+        }
+        String result = "";
+        for (int i = 0; i <arr.length; i++ ) {
+            result += arr[i] + " ";
+        }
+        System.out.println(result);
+    }
+}
