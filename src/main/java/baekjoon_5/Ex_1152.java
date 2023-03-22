bfaad65c199f147ba451164d433ddce30fe332da
chlwnstkd <jun1234755@naver.com>
2023-03-22 11:07:20 +0900

    commit push

---
+++ src/main/java/baekjoon_5/Ex_1152.java
@@ -0,0 +1,16 @@
+package baekjoon_5;
+
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Ex_1152 {
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+
+        String str = s.nextLine();
+
+        StringTokenizer st = new StringTokenizer(str," ");
+
+        System.out.println(st.countTokens());
+    }
+}
