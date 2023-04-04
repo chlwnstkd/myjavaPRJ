618bedb5e7bdbef3b797154bdc7755ceff1263d2
chlwnstkd <jun1234755@naver.com>
2023-04-04 21:58:19 +0900

    2023-04-03

---
+++ src/main/java/org/example/Ex23.java
@@ -0,0 +1,17 @@
+package org.example;
+import java.util.Scanner;
+public class Ex23 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.printf("문자열을 입력하세요 : ");
+        String str = s.nextLine();
+        String tt = "";
+
+        int count = str.length();
+
+        for (int i = 0; i < count; i++){
+            tt += str.charAt(count - (i+1));
+        }
+        System.out.printf("내용을 거꾸로 출력 ==> %s \n", tt);
+    }
+}
