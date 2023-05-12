3af270fb75cd69276d46b7f7dcb4f2be7e1ea390
chlwnstkd <jun1234755@naver.com>
2023-05-12 19:47:41 +0900

    2023-05-12

--- src/main/java/sort/quick.java
+++ src/main/java/sort/quick.java
@@ -5,17 +5,22 @@ import java.util.Scanner;
 public class quick {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
+
         int n = s.nextInt();
         int[] arr = new int[n];
+        int k,j,p,count=0,coun=0;
         for(int i = 0; i < arr.length; i++) {
             arr[i] = s.nextInt();
         }
         while(true) {
-            for(int i = 1; i <n;) {
-                i = 1;
-                if (arr[i] < arr[i-1]) {
-                    i++;
-                }
+            p = arr[0];
+            k = arr[count];
+            j = arr[n-1-coun];
+            if(k>=p)
+                count++;
+            if(j<=p)
+                coun++;
+            else {
 
             }
         }
