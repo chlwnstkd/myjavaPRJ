9aa0e77f2a85faf0e11f92fbdbe0d5ca99a4de13
chlwnstkd <jun1234755@naver.com>
2023-05-15 13:25:56 +0900

    2023-05-15

--- src/main/java/sort/quick.java
+++ src/main/java/sort/quick.java
@@ -1,28 +1,37 @@
 package sort;
 
-import java.util.Scanner;
-
 public class quick {
-    public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
 
-        int n = s.nextInt();
-        int[] arr = new int[n];
-        int k,j,p,count=0,coun=0;
-        for(int i = 0; i < arr.length; i++) {
-            arr[i] = s.nextInt();
-        }
-        while(true) {
-            p = arr[0];
-            k = arr[count];
-            j = arr[n-1-coun];
-            if(k>=p)
-                count++;
-            if(j<=p)
-                coun++;
-            else {
 
+    public static void quickSort(int[] arr) {
+        sort(arr, 0, arr.length - 1);
+    }
+
+    private static void sort(int[] arr, int low, int high) {
+        if (low >= high) return;
+
+        int mid = partition(arr, low, high);
+        sort(arr, low, mid - 1);
+        sort(arr, mid, high);
+    }
+
+    private static int partition(int[] arr, int low, int high) {
+        int pivot = arr[(low + high) / 2];
+        while (low <= high) {
+            while (arr[low] < pivot) low++;
+            while (arr[high] > pivot) high--;
+            if (low <= high) {
+                swap(arr, low, high);
+                low++;
+                high--;
             }
         }
+        return low;
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
     }
 }
