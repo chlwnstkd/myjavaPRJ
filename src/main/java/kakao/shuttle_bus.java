18f5769420e80375093bd9f8d5cc18c7d6c1c909
chlwnstkd <jun1234755@naver.com>
2023-05-25 16:39:39 +0900

    2023-05-25 -3일차

--- src/main/java/kakao/shuttle_bus.java
+++ src/main/java/kakao/shuttle_bus.java
@@ -6,10 +6,10 @@ import java.util.Scanner;
 public class shuttle_bus {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
+        String timetable = s.nextLine();
         int n = s.nextInt();
         int t = s.nextInt();
         int m = s.nextInt();
-        String timetable = s.next();
         timetable = timetable.replace("[", "");
         timetable = timetable.replace("]", "");
         timetable = timetable.replace("”", "");
@@ -25,36 +25,35 @@ public class shuttle_bus {
             }
         }
         int[] people = new int[temp.length];
+        int[] people2 = new int[temp.length];
         for(int i = 0; i < temp.length; i++) {
             people[i] = time[i][0]*60 + time[i][1];
+            people2[i] = people[i];
         }
         int d = t;
         int a = t;
         int c = n;
-        while(n != 0) {
-            int b = m;
+        int b = m;
+        Arrays.sort(people);
+        for(int k = 0; k < n; k++) {
             for(int i = 0; i < m; i++) {
                 for(int j = 0; j < people.length; j ++) {
                     if(people[j] < 9*60 + t) {
                         people[j] = 24 * 60;
                         b--;
+                        break;
                     }
                 }
             }
-            if(n == 1) {
-                m = b-1;
-            }
             t += a;
-            n--;
         }
-        Arrays.sort(people);
+        Arrays.sort(people2);
         int answer2[] = new int[2];
-        if(m == 0) {
-            answer2[0] = people[0]/60;
-            answer2[1] = people[1]/60;
+        if(b <= 0) {
+            answer2[0] = (people2[c*m-2]-1)/60;
+            answer2[1] = (people2[c*m-2]-1)%60;
         }else {
             int answer1 = (60 * 9) + (d*(c-1));
-            System.out.println(answer1);
             answer2[0] = answer1/60;
             answer2[1] = answer1%60;
         }
