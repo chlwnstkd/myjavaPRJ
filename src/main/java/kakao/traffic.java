62b01f1e2138073c8e1ed0246754b446f55479f6
chlwnstkd <jun1234755@naver.com>
2023-06-05 08:54:55 +0900

    2023-06-05

--- src/main/java/kakao/traffic.java
+++ src/main/java/kakao/traffic.java
@@ -1,53 +1,58 @@
 package kakao;
 
-import java.util.Arrays;
-import java.util.Scanner;
+import java.time.LocalTime;
 
 public class traffic {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String aaa = sc.nextLine();
-        aaa = aaa.replace("[", "");
-        aaa = aaa.replace("]", "");
-        aaa = aaa.replace("”", "");
-        aaa = aaa.replace("“", "");
-        String[] enter = aaa.split(",");
-        double[] endtime = new double[enter.length];
-        double[] starttime = new double[enter.length];
-        double[] runtime = new double[enter.length];
-        for (int i = 0; i < enter.length; i++) {
-            int a = enter[i].lastIndexOf(":");
-            int b = enter[i].indexOf("s");
-            System.out.println(enter[i].substring(a+1, a+7));
-            double s = Double.parseDouble(enter[i].substring(a+1, a+7));
-            float m = Math.round(Float.parseFloat(enter[i].substring(a-2, a)));
-            float h = Math.round(Float.parseFloat(enter[i].substring(a-5, a-3)));
-            endtime[i] = s * 60 * 60000 + m * 60000 + h;
-            runtime[i] = Math.round((Float.parseFloat(enter[i].substring(a+8, b)) * 1000) / 1000.00);
-            starttime[i] = endtime[i] - runtime[i] + (float) 0.001;
-        }
-        System.out.println(Arrays.toString(starttime));
-        System.out.println(Arrays.toString(endtime));
-        System.out.println(Arrays.toString(runtime));
-        int max = 0;
-        for(int i = 0; i < starttime.length; i++) {
-            for (int j = 0; j < starttime.length; j++) {
-                for (float k = 0; k < runtime[i]; k+=0.001) {
-                    int cnt = 0;
-                    double temp = starttime[i];
-                    if (starttime[i] > starttime[j] && starttime[i] + 1 < starttime[j]) {
-                        cnt++;
-                    } else if (starttime[i] < starttime[j] && starttime[i] + k > starttime[j]) {
-                        cnt++;
-                    } else if (starttime[i] < endtime[j] && starttime[i] + k > endtime[j]) {
-                    }
-                    if (max < cnt) {
-                        max = cnt;
-                        System.out.println(max);
-                    }
+
+        String[] lines = {
+                "2016-09-15 03:10:33.020 0.011s",
+                "2016-09-15 03:10:34.123 1.123s",
+                "2016-09-15 03:10:45.111 2.222s"
+        };
+
+        int maxThroughput = solution(lines);
+        System.out.println("초당 최대 처리량: " + maxThroughput);
+    }
+
+    public static int solution(String[] lines) {
+        int maxThroughput = 0;
+
+        for (int i = 0; i < lines.length; i++) {
+            String[] log = lines[i].split(" ");
+            String[] responseTime = log[1].split(":");
+            int hours = Integer.parseInt(responseTime[0]);
+            int minutes = Integer.parseInt(responseTime[1]);
+            double seconds = Double.parseDouble(responseTime[2]);
+            double processingTime = Double.parseDouble(log[2].substring(0, log[2].length() - 1));
+
+            LocalTime startTime = LocalTime.of(hours, minutes, (int) seconds).minusSeconds((long) (processingTime - 0.001));
+            LocalTime endTime = LocalTime.of(hours, minutes, (int) seconds);
+            int count = 0;
+
+            for (int j = i; j < lines.length; j++) {
+                String[] nextLog = lines[j].split(" ");
+                String[] nextResponseTime = nextLog[1].split(":");
+                int nextHours = Integer.parseInt(nextResponseTime[0]);
+                int nextMinutes = Integer.parseInt(nextResponseTime[1]);
+                double nextSeconds = Double.parseDouble(nextResponseTime[2]);
+                double nextProcessingTime = Double.parseDouble(nextLog[2].substring(0, nextLog[2].length() - 1));
+
+                LocalTime nextStartTime = LocalTime.of(nextHours, nextMinutes, (int) nextSeconds).minusSeconds(3);
+                LocalTime nextEndTime = LocalTime.of(nextHours, nextMinutes, (int) nextSeconds);
+
+                if (nextStartTime.isBefore(endTime) || nextStartTime.equals(endTime)) {
+                    count++;
+                } else if (nextEndTime.isBefore(startTime)) {
+                    break;
                 }
             }
+
+            if (count > maxThroughput) {
+                maxThroughput = count;
+            }
         }
-        System.out.println(max);
+
+        return maxThroughput;
     }
 }
